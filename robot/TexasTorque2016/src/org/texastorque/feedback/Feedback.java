38a5e1392f46553bb1708b7bc1dade692856aeef
Noviv <matthewwebb65@yahoo.com>
2016-01-15 17:42:03 -0600

    removed static calls to feedback instance

--- robot/TexasTorque2016/src/org/texastorque/feedback/Feedback.java
+++ robot/TexasTorque2016/src/org/texastorque/feedback/Feedback.java
@@ -1,22 +1,31 @@
 package org.texastorque.feedback;
 
+import org.texastorque.feedback.vision.VisionManager;
+
 public class Feedback {
 	
 	private static Feedback instance;
+	
+	private VisionManager visionManager;
+	
+	public Feedback() {
+		visionManager = VisionManager.getInstance();
+	}
 	
 	//sensors
 	
 	//values
 	
-	private void update0() {
+	public void update() {
+		visionManager.update();
+	}
+	
+	public void pushToDashboard() {
+		visionManager.pushToDashboard();
 	}
 	
 	//singleton
 	public static Feedback getInstance() {
 		return instance == null ? instance = new Feedback() : instance;
-	}
-	
-	public static void update() {
-		getInstance().update0();
 	}
 }
