bc423ab76da98c18cc5f0ff7d8a6e9d13e48e2b2
Noviv <matthewwebb65@yahoo.com>
2016-01-16 11:23:45 -0600

    added vision yaw and pitch calculations

--- robot/TexasTorque2016/src/org/texastorque/constants/Constants.java
+++ robot/TexasTorque2016/src/org/texastorque/constants/Constants.java
@@ -4,5 +4,12 @@ import org.texastorque.torquelib.util.Parameters.Constant;
 
 public class Constants {
 
+	// auto
 	public static final Constant DRIVE_FORWARD_AUTO_DISTANCE = new Constant("A_DriveForwardDistance", 0.0);
+	
+	//vision
+	public static final Constant CAMERA_MAX_PITCH_FOV_ANGLE = new Constant("V_CamMaxYFOVAngle", 0.0);
+	public static final Constant CAMERA_MIN_PITCH_FOV_ANGLE = new Constant("V_CamMinYFOVAngle", 0.0);
+	public static final Constant CAMERA_MAX_YAW_FOV_ANGLE = new Constant("V_CamMaxXFOVAngle", 0.0);
+	public static final Constant CAMERA_MIN_YAW_FOV_ANGLE = new Constant("V_CamMinXFOVAngle", 0.0);
 }
--- robot/TexasTorque2016/src/org/texastorque/feedback/vision/VisionManager.java
+++ robot/TexasTorque2016/src/org/texastorque/feedback/vision/VisionManager.java
@@ -1,5 +1,7 @@
 package org.texastorque.feedback.vision;
 
+import org.texastorque.constants.Constants;
+
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -7,26 +9,42 @@ public class VisionManager {
 
 	private static VisionManager instance;
 
-	private NetworkTable visionTable;
-
+	private final NetworkTable visionTable;
+	private final double pitchAngleRange;
+	private final double yawAngleRange;
+	
+	//input values
 	private double centerX;
 	private double centerY;
+	private double width;
+	private double height;
+	
+	//calculated values
+	private double necessaryPitch;
+	private double necessaryYaw;
 
 	public VisionManager() {
 		visionTable = NetworkTable.getTable("visionReport");
+		pitchAngleRange = Constants.CAMERA_MAX_PITCH_FOV_ANGLE.getDouble() - Constants.CAMERA_MIN_PITCH_FOV_ANGLE.getDouble();
+		yawAngleRange = Constants.CAMERA_MAX_YAW_FOV_ANGLE.getDouble() - Constants.CAMERA_MIN_YAW_FOV_ANGLE.getDouble();
 	}
 
 	public void update() {
 		centerX = visionTable.getNumber("centerX", 0.0);
 		centerY = visionTable.getNumber("centerY", 0.0);
+		width = visionTable.getNumber("width", 0.0);
+		height = visionTable.getNumber("height", 0.0);
+		
+		necessaryPitch = ((centerY / height) * pitchAngleRange) + pitchAngleRange;
+		necessaryYaw = ((centerX / width) * yawAngleRange) + yawAngleRange;
 	}
-
-	public double getCenterX() {
-		return centerX;
+	
+	public double getNecessaryPitch() {
+		return necessaryPitch;
 	}
-
-	public double getCenterY() {
-		return centerY;
+	
+	public double getNecessaryYaw() {
+		return necessaryYaw;
 	}
 
 	public void pushToDashboard() {
