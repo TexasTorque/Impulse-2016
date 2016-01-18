bfdc35eef2c4aadb265d4533b132952367ec77e4
Noviv <matthewwebb65@yahoo.com>
2016-01-18 10:48:06 -0600

    added output for drivetrain motors

--- robot/TexasTorque2016/src/org/texastorque/output/RobotOutput.java
+++ robot/TexasTorque2016/src/org/texastorque/output/RobotOutput.java
@@ -1,15 +1,48 @@
 package org.texastorque.output;
 
+import org.texastorque.torquelib.component.TorqueMotor;
+
+import edu.wpi.first.wpilibj.VictorSP;
+
 public class RobotOutput {
 
 	private static RobotOutput instance;
 
 	private static final boolean OUTPUT_MUTED = false;
 
+	private TorqueMotor leftDriveTop;
+	private TorqueMotor leftDriveMid;
+	private TorqueMotor leftDriveBot;
+	private TorqueMotor rightDriveTop;
+	private TorqueMotor rightDriveMid;
+	private TorqueMotor rightDriveBot;
+
+	public RobotOutput() {
+		leftDriveTop = new TorqueMotor(new VictorSP(0), false);
+		leftDriveMid = new TorqueMotor(new VictorSP(0), false);
+		leftDriveBot = new TorqueMotor(new VictorSP(0), false);
+
+		rightDriveTop = new TorqueMotor(new VictorSP(0), true);
+		rightDriveMid = new TorqueMotor(new VictorSP(0), true);
+		rightDriveBot = new TorqueMotor(new VictorSP(0), true);
+	}
+
 	public void setDriveSpeeds(double left, double right) {
 		if (OUTPUT_MUTED) {
+			leftDriveTop.set(0.0);
+			leftDriveMid.set(0.0);
+			leftDriveBot.set(0.0);
+			rightDriveTop.set(0.0);
+			rightDriveMid.set(0.0);
+			rightDriveBot.set(0.0);
 			return;
 		}
+		leftDriveTop.set(left);
+		leftDriveMid.set(left);
+		leftDriveBot.set(left);
+		rightDriveTop.set(right);
+		rightDriveMid.set(right);
+		rightDriveBot.set(right);
 	}
 
 	// singleton
