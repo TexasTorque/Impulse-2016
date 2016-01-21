c0e092e47b4abab3b2cb729fc5e3eab9e7eced72
DanielHurin <DanielHurin@gmail.com>
2016-01-21 16:29:48 -0600

    pre-work for autonomous completed in the subsystem and drivebase classes

--- robot/TexasTorque2016/src/org/texastorque/subsystem/Drivebase.java
+++ robot/TexasTorque2016/src/org/texastorque/subsystem/Drivebase.java
@@ -3,6 +3,7 @@ package org.texastorque.subsystem;
 import org.texastorque.constants.Constants;
 import org.texastorque.torquelib.controlLoop.TorquePV;
 import org.texastorque.torquelib.controlLoop.TorqueTMP;
+import org.texastorque.torquelib.util.TorqueMathUtil;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -15,6 +16,17 @@ public class Drivebase extends Subsystem {
 
 	private double leftSpeed;
 	private double rightSpeed;
+	
+	// sensor
+	private double leftPosition;
+	private double rightPosition;
+	
+	private double leftVelocity;
+	private double rightVelocity;
+	
+	private double leftAcceleration;
+	private double rightAcceleration;
+	
 	
 	// profiling variables
 	
@@ -40,6 +52,9 @@ public class Drivebase extends Subsystem {
 		leftPV = new TorquePV();
 		rightPV = new TorquePV();
 		
+		feedback.resetDriveEncoders();
+		setpoint = 0.0;
+		
 		leftPV.setGains(Constants.D_LEFT_PV_P.getDouble(), 
 						Constants.D_LEFT_PV_V.getDouble(),
 						Constants.D_LEFT_PV_ffP.getDouble(),
@@ -56,13 +71,54 @@ public class Drivebase extends Subsystem {
 	}
 
 	public void run() {
-		leftSpeed = input.getLeftDriveSpeed();
-		rightSpeed = input.getRightDriveSpeed();
+		
+		leftPosition = feedback.getLeftDrivePosition();
+		rightPosition = feedback.getRightDrivePosition();
+		
+		leftVelocity = feedback.getLeftDriveVelocity();
+		rightVelocity = feedback.getRightDriveVelocity();
+		
+		leftAcceleration = feedback.getLeftDriveAcceleration();
+		rightAcceleration = feedback.getRightDriveAcceleration();
+		
+		if(driverStation.isAutonomous()){
+			if(input.isOverride()){
+				leftSpeed = input.getLeftDriveSpeed();
+				rightSpeed = input.getRightDriveSpeed();
+			} else {
+				setpoint = input.getDrivebaseSetpoint();
+				if (setpoint != previousSetpoint){
+					previousSetpoint = setpoint;
+					feedback.resetDriveEncoders();
+					profile.generateTrapezoid(setpoint, 0.0, 0.0);
+				}
+				
+				double dt = Timer.getFPGATimestamp() - prevTime;
+				prevTime = Timer.getFPGATimestamp();
+				profile.calculateNextSituation(dt);
+				
+				targetPosition = profile.getCurrentPosition();
+				targetVelocity = profile.getCurrentVelocity();
+				targetAcceleration = profile.getCurrentAcceleration();
+				
+				leftSpeed = leftPV.calculate(profile, leftPosition, leftVelocity);
+				rightSpeed = rightPV.calculate(profile, rightPosition, rightVelocity);
+			}
+		} else {
+		
+			leftSpeed = input.getLeftDriveSpeed();
+			rightSpeed = input.getRightDriveSpeed();
+		
+		}
 		
 		output();
 	}
 
 	protected void output() {
+		
+		leftSpeed = TorqueMathUtil.constrain(leftSpeed, MAX_SPEED);
+		rightSpeed = TorqueMathUtil.constrain(rightSpeed, MAX_SPEED);
+		
 		output.setDriveSpeeds(leftSpeed, rightSpeed);
 	}
 
--- robot/TexasTorque2016/src/org/texastorque/subsystem/Subsystem.java
+++ robot/TexasTorque2016/src/org/texastorque/subsystem/Subsystem.java
@@ -4,15 +4,19 @@ import org.texastorque.feedback.Feedback;
 import org.texastorque.input.Input;
 import org.texastorque.output.RobotOutput;
 
+import edu.wpi.first.wpilibj.DriverStation;
+
 public abstract class Subsystem {
 
 	protected Input input;
 	protected Feedback feedback;
 	protected RobotOutput output;
+	protected DriverStation driverStation;
 	
 	public Subsystem(){
 		feedback = Feedback.getInstance();
 		output = RobotOutput.getInstance();
+		driverStation = DriverStation.getInstance();
 	}
 
 	public final void setInput(Input _input) {
