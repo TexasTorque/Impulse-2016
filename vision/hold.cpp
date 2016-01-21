c5b8bed250c06d97f01d7efff8dc314712f38f20
Noviv <matthewwebb65@yahoo.com>
2016-01-21 08:30:31 -0600

    fixed findContours runtime error

---
+++ vision/hold.cpp
@@ -0,0 +1,28 @@
+#include <opencv2\imgproc.hpp>
+#include <opencv2\imgproc\imgproc_c.h>
+#include <opencv2\highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+	VideoCapture camera(0);
+
+	Mat frame;
+	vector<Vec4i> hierarchy;
+	vector<vector<Point>> contours;
+
+	namedWindow("window", CV_WINDOW_AUTOSIZE);
+
+	while (true) {
+		camera >> frame;
+
+		inRange(frame, Scalar(0, 100, 0), Scalar(255, 255, 255), frame);
+
+		findContours(frame, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+		imshow("window", frame);
+		if (waitKey(30) >= 0) break;
+	}
+	return 0;
+}
