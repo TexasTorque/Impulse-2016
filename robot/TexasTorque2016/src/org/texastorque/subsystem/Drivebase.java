f9866a684336a4aa51875a7f24ecf6c18372003f
Noviv <matthewwebb65@yahoo.com>
2016-02-10 08:36:12 -0600

    removed resetSetpoints

--- robot/TexasTorque2016/src/org/texastorque/subsystem/Drivebase.java
+++ robot/TexasTorque2016/src/org/texastorque/subsystem/Drivebase.java
@@ -55,8 +55,6 @@ public class Drivebase extends Subsystem {
 
 	@Override
 	public void init() {
-		input.resetSetpoints();
-
 		// linear
 		profile = new TorqueTMP(Constants.D_MAX_VELOCITY.getDouble(), Constants.D_MAX_ACCELERATION.getDouble());
 		leftPV = new TorquePV();
