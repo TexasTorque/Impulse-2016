094d42f0b9f606258d1b790f16fc1e9a2cb6055a
DanielHurin <DanielHurin@gmail.com>
2016-02-18 16:28:46 -0600

    created the TorqueMovementAverage which averages numbers

---
+++ robot/TexasTorque2016/src/org/texastorque/torquelib/util/TorqueMovementAverage.java
@@ -0,0 +1,32 @@
+package org.texastorque.torquelib.util;
+
+import java.util.ArrayList;
+
+public class TorqueMovementAverage {
+	
+	private int numberOfMoves = 0;
+	private ArrayList<Double> previousMoves = new ArrayList(100);	
+	
+	public double getAverage(){
+		int denominator = 0;
+		double numerator = 0;
+		for(double move : previousMoves){
+			if(move != 0.0)
+				denominator++;
+			numerator += move;
+		}
+		return numerator / denominator;
+	}
+	
+	public void addMove(double move){
+		if(numberOfMoves < previousMoves.size()){
+			previousMoves.add(move);
+			numberOfMoves++;
+		}
+		else {
+			previousMoves.remove(0);
+			previousMoves.add(move);
+		}
+	}
+
+}
