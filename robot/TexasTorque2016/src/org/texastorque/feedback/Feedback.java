2f1de00f9da1e3e2b16a48796586de81edabad5f
Noviv <matthewwebb65@yahoo.com>
2016-03-17 08:56:13 -0600

    switched to SPI gyro & added raw pot values to dashboard

--- robot/TexasTorque2016/src/org/texastorque/feedback/Feedback.java
+++ robot/TexasTorque2016/src/org/texastorque/feedback/Feedback.java
@@ -1,15 +1,13 @@
 package org.texastorque.feedback;
 
 import org.texastorque.constants.Constants;
-import org.texastorque.constants.PortsBravo;
+import org.texastorque.constants.Ports;
 import org.texastorque.torquelib.component.TorqueEncoder;
-import org.texastorque.torquelib.component.TorqueGyro;
 import org.texastorque.torquelib.component.TorquePotentiometer;
 import org.texastorque.torquelib.util.TorqueMathUtil;
 
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Feedback {
 
@@ -23,19 +21,17 @@ public class Feedback {
 
 	// private I2C lidar;
 
-	// private ADXRS450_Gyro gyro;
-	private TorqueGyro gyro;
+	private ADXRS450_Gyro gyro;
 
 	private TorqueEncoder leftDriveEncoder;
 	private TorqueEncoder rightDriveEncoder;
-
 	private TorqueEncoder flywheelEncoder;
 
 	private TorquePotentiometer tiltPot;
 	private TorquePotentiometer compressionPot;
+	private TorquePotentiometer mechanismPot;
 
-	private DigitalInput compressionSensor;
-	private DigitalInput tiltLimitSwitch;
+//	private DigitalInput compressionSensor;
 
 	// drivebase values
 	private double leftDrivePosition;
@@ -53,38 +49,41 @@ public class Feedback {
 	private double flywheelVelocity;
 
 	private double tiltAngle;
-
-	// other values
+	private double tiltPotRaw;
 
 	private double lidarDistance;
-	
-	private boolean tiltLimitSwitchActive;
+
+	// a mechanism values
+	private double mechanismAngle;
+	private double mechanismPotRaw;
 
 	public Feedback() {
 		vision = VisionFeedback.getInstance();
 
 		// lidar = new I2C(I2C.Port.kMXP, 62);
 
-		// gyro = new ADXRS450_Gyro();
-		gyro = new TorqueGyro(PortsBravo.TURNING_GYRO_A, PortsBravo.TURNING_GYRO_B);
-		leftDriveEncoder = new TorqueEncoder(PortsBravo.DRIVE_LEFT_ENCODER_A, PortsBravo.DRIVE_LEFT_ENCODER_B, false,
+		gyro = new ADXRS450_Gyro();
+		leftDriveEncoder = new TorqueEncoder(Ports.DRIVE_LEFT_ENCODER_A, Ports.DRIVE_LEFT_ENCODER_B, true,
 				EncodingType.k4X);
-		rightDriveEncoder = new TorqueEncoder(PortsBravo.DRIVE_RIGHT_ENCODER_A, PortsBravo.DRIVE_RIGHT_ENCODER_B, false,
+		rightDriveEncoder = new TorqueEncoder(Ports.DRIVE_RIGHT_ENCODER_A, Ports.DRIVE_RIGHT_ENCODER_B, false,
 				EncodingType.k4X);
 
-		flywheelEncoder = new TorqueEncoder(PortsBravo.FLYWHEEL_ENCODER_A, PortsBravo.FLYWHEEL_ENCODER_B, true,
-				EncodingType.k4X);
-		tiltPot = new TorquePotentiometer(PortsBravo.TILT_POT_PORT);
+		flywheelEncoder = new TorqueEncoder(Ports.FLYWHEEL_ENCODER_A, Ports.FLYWHEEL_ENCODER_B, true, EncodingType.k4X);
+		tiltPot = new TorquePotentiometer(Ports.TILT_POT_PORT);
 
-		compressionPot = new TorquePotentiometer(PortsBravo.COMPRESSION_POT);
-		compressionSensor = new DigitalInput(PortsBravo.COMPRESSON_SENSOR);
+//		compressionSensor = new DigitalInput(Ports.COMPRESSON_SENSOR);
 
-		tiltLimitSwitch = new DigitalInput(PortsBravo.TILT_LIMIT_SWITCH);
+		compressionPot = new TorquePotentiometer(Ports.COMPRESSION_POT);
+		mechanismPot = new TorquePotentiometer(Ports.A_MECHANISM_POT);
 	}
 
 	public void init() {
 		tiltPot.setInputRange(Constants.S_TILT_MIN_VOLTAGE.getDouble(), Constants.S_TILT_MAX_VOLTAGE.getDouble());
 		tiltPot.setPositionRange(Constants.S_TILT_MIN_ANGLE.getDouble(), Constants.S_TILT_MAX_ANGLE.getDouble());
+
+		mechanismPot.setInputRange(Constants.AMECH_MIN_VOLTAGE.getDouble(), Constants.AMECH_MAX_VOLTAGE.getDouble());
+		mechanismPot.setPositionRange(Constants.AMECH_MIN_ANGLE.getDouble(), Constants.AMECH_MAX_ANGLE.getDouble());
+
 	}
 
 	public void update() {
@@ -105,16 +104,16 @@ public class Feedback {
 
 		flywheelVelocity = flywheelEncoder.getRate() * FLYWHEEL_CONVERSION;
 
-		SmartDashboard.putNumber("POTVALUE", tiltPot.getRaw());
-
 		tiltAngle = tiltPot.getPosition();
+		tiltPotRaw = tiltPot.getRaw();
+
+		mechanismAngle = mechanismPot.getPosition();
+		mechanismPotRaw = mechanismPot.getRaw();
 
 		// byte[] temp = new byte[8];
 		// lidar.readOnly(temp, 8);
 		// lidarDistance = (double) temp[0];
 		// lidarDistance = ByteBuffer.wrap(temp).getDouble();
-		
-		tiltLimitSwitchActive = tiltLimitSwitch.get();
 
 		vision.calc();
 	}
@@ -173,16 +172,25 @@ public class Feedback {
 		return tiltAngle;
 	}
 
+	public double getTiltPotRaw() {
+		return tiltPotRaw;
+	}
+
+	public double getMechanismAngle() {
+		return mechanismAngle;
+	}
+
+	public double getMechanismPotRaw() {
+		return mechanismPotRaw;
+	}
+
 	public double getCompressionValue() {
 		return compressionPot.getRaw();
 	}
 
 	public boolean isCompressionTestReady() {
-		return compressionSensor.get();
-	}
-	
-	public boolean isTiltLimitSwitchActive() {
-		return tiltLimitSwitchActive;
+//		return compressionSensor.get();
+		return false;
 	}
 
 	public double getLidarDistance() {
