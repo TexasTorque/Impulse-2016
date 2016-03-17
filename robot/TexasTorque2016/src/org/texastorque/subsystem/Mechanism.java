b6ffb4fddc2b1b55249b3ac4fc17966265f6435d
Noviv <matthewwebb65@yahoo.com>
2016-03-17 09:05:06 -0600

    added mechanism setpoints and PID tuning

--- robot/TexasTorque2016/src/org/texastorque/subsystem/Mechanism.java
+++ robot/TexasTorque2016/src/org/texastorque/subsystem/Mechanism.java
@@ -1,21 +1,51 @@
 package org.texastorque.subsystem;
 
+import org.texastorque.constants.Constants;
+import org.texastorque.torquelib.controlLoop.TorquePID;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Mechanism extends Subsystem {
-	
+
 	private static Mechanism instance;
-	
+
 	private double mechanismSpeed;
+
+	private double mechanismSetpoint;
+	private double mechanismAngle;
+	private double mechanismPotRaw;
+	private boolean mechanismHold;
+
+	// profiling
+	private TorquePID mechanismPID;
 
 	@Override
 	public void init() {
+		mechanismPID = new TorquePID();
+		mechanismPID.setPIDGains(Constants.AMECH_P.getDouble(), Constants.AMECH_I.getDouble(),
+				Constants.AMECH_D.getDouble());
+		mechanismPID.setMaxOutput(1.0);
 	}
 
 	@Override
 	public void run() {
-		mechanismSpeed = input.getMechanismSpeed();
-		
+		mechanismAngle = feedback.getMechanismAngle();
+		mechanismPotRaw = feedback.getMechanismPotRaw();
+
+//		if (input.isOverride()) {
+			mechanismSpeed = input.getMechanismOverrideSpeed() / 2.0;
+//		} else {
+//			mechanismHold = input.isMechanismHold();
+//			if (mechanismHold) {
+//				mechanismSetpoint = Constants.AMECH_HOLD_SETPOINT.getDouble();
+//			} else {
+//				mechanismSetpoint = input.getMechanismSetpoint();
+//			}
+//			mechanismPID.setSetpoint(mechanismSetpoint);
+
+//			mechanismSpeed = mechanismPID.calculate(mechanismAngle);
+//		}
+
 		output();
 	}
 
@@ -26,10 +56,15 @@ public class Mechanism extends Subsystem {
 
 	@Override
 	public void pushToDashboard() {
+		SmartDashboard.putNumber("MechanismAngle", mechanismAngle);
+		SmartDashboard.putNumber("MechanismPotRaw", mechanismPotRaw);
+		SmartDashboard.putNumber("MechanismSetpoint", mechanismSetpoint);
+		SmartDashboard.putBoolean("MechanismHold", mechanismHold);
+
 		SmartDashboard.putNumber("MechanismSpeed", mechanismSpeed);
 	}
-	
-	//singleton
+
+	// singleton
 	public static Mechanism getInstance() {
 		return instance == null ? instance = new Mechanism() : instance;
 	}
